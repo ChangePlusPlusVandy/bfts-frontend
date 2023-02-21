2951b9b05137ff26966feb2b280052b11400253a
BobbyMi11er <BobbyMi11er@users.noreply.github.com>
2023-02-21 23:32:09 +0000

    Run Prettier

--- firebase.js
+++ firebase.js
@@ -4,19 +4,18 @@ import { initializeApp } from 'firebase/app';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 import 'firebase/auth';
-import { getAuth } from "firebase/auth";
-import {API_KEY, AUTH_DOMAIN, PROJECT_ID, STORAGE_BUCKET, MESSAGING_SENDER_ID, FIREBASE_APP_ID} from '@env';
-
+import { getAuth } from 'firebase/auth';
+import { API_KEY, AUTH_DOMAIN, PROJECT_ID, STORAGE_BUCKET, MESSAGING_SENDER_ID, FIREBASE_APP_ID } from '@env';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  // Insert secrets here
-  apiKey: API_KEY,
-  authDomain: AUTH_DOMAIN,
-  projectId: PROJECT_ID,
-  storageBucket: STORAGE_BUCKET,
-  messagingSenderId: MESSAGING_SENDER_ID,
-  appId: FIREBASE_APP_ID
+	// Insert secrets here
+	apiKey: API_KEY,
+	authDomain: AUTH_DOMAIN,
+	projectId: PROJECT_ID,
+	storageBucket: STORAGE_BUCKET,
+	messagingSenderId: MESSAGING_SENDER_ID,
+	appId: FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
