8cc3f860477dcc6f131aa5ccf2e3708c856730e9
Bobby Mi11er <bobby@tybeemillers.com>
2023-03-06 23:20:51 -0600

    Create profile card

---
+++ components/Profile/ProfileCard.js
@@ -0,0 +1,19 @@
+import { View, Text } from "react-native";
+import cardStyles from "./ProfileCardStyles";
+
+export default function ProfileCard({ profile }) {
+    return (
+        <View style={cardStyles.main}>
+            <View style={cardStyles.nameView}>
+                <Text>
+                    <Text style={{color: 'black', fontWeight: '600', fontSize: 20}}>{profile.name}</Text>
+                    <Text style={{color: 'black', fontWeight: '200', fontSize: 15}}>  {profile.pronouns}</Text>
+                </Text>
+            </View>
+            <View style={{marginLeft:20, marginBottom: 40}}>
+                <Text>{profile.location}</Text>
+                <Text>{profile.livingSituation}</Text>
+            </View>
+        </View>
+    );
+}
