b3aff6c76c865b3d549f8472b0edb804e92e227f
Bobby Mi11er <bobby@tybeemillers.com>
2023-03-07 20:57:33 -0600

    Create custom headers

--- components/home/Home.js
+++ components/home/Home.js
@@ -1,8 +1,11 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Main from '../main/Main';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import Community from '../community/Community';
+import Header from '../header/Header';
+import { useNavigation } from '@react-navigation/native';
+import { BFTS_BLUE } from '../../constants';
 
 const Tab = createBottomTabNavigator();
 
@@ -20,11 +23,28 @@ const Home = () => {
 					}
 					return <Ionicons name={iconName} size={size} color={color} />;
 				},
-				tabBarActiveTintColor: 'blue',
+				tabBarActiveTintColor: {BFTS_BLUE},
 				tabBarInactiveTintColor: 'gray',
 			})}>
-			<Tab.Screen name="Main" component={Main} options={{ headerShown: false }} />
-			<Tab.Screen name="Community" component={Community} options={{ headerShown: false }} />
+			<Tab.Screen name="Main" component={Main} options={{ 
+				headerTitle: () => <Header name="main"/>,
+				headerLeft: () => 
+					<View>
+						<TouchableOpacity style={{marginLeft: 15}}>
+							<Ionicons name="images-outline" size={25} color={BFTS_BLUE}></Ionicons>
+						</TouchableOpacity>
+					</View>
+				
+			}} />
+			<Tab.Screen name="Community" component={Community} options={{ 
+				headerTitle: () => <Header name="community"/>,
+				headerLeft: () => 
+					<View>
+						<TouchableOpacity style={{marginLeft: 15}}>
+							<Ionicons name="chevron-back-outline" size={25} color={BFTS_BLUE}></Ionicons>
+						</TouchableOpacity>
+					</View>
+			}} />
 		</Tab.Navigator>
 	);
 };
