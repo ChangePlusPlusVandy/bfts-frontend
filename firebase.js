7af6f7ba22124a58f0025420e9af206d307e33f0
Umphaipun Atipunumphai <panda3141592@gmail.com>
2023-03-08 20:27:25 -0600

    remove config

--- firebase.js
+++ firebase.js
@@ -9,12 +9,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-	apiKey: 'AIzaSyBbnwgqvj0WE7s5ldM7zzv_4IYGKk3YxQs',
-	authDomain: 'bfts-e5301.firebaseapp.com',
-	projectId: 'bfts-e5301',
-	storageBucket: 'bfts-e5301.appspot.com',
-	messagingSenderId: '682538519243',
-	appId: '1:682538519243:web:385c232049722288f6a94e',
+	// Insert secrets here
 };
 
 // Initialize Firebase
