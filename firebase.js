d2d38f76b99221a28eef3efc3fcf02931a870ab5
Ellie Li <ellieli@Ellies-MacBook-Pro.local>
2023-04-07 01:47:50 -0500

    merged

--- firebase.js
+++ firebase.js
@@ -9,7 +9,13 @@ import { getAuth } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  // Insert secrets here
+	// Insert secrets here
+	apiKey: process.env.API_KEY,
+	authDomain: process.env.AUTH_DOMAIN,
+	projectId: process.env.PROJECT_ID,
+	storageBucket: process.env.STORAGE_BUCKET,
+	messagingSenderId: process.env.MESSAGING_SENDER_ID,
+	appId: process.env.FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
