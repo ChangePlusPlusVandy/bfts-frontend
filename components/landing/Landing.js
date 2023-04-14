082501eda886808c8c24cd8f625ada8f5cbab02f
Bobby Mi11er <bobby@tybeemillers.com>
2023-04-14 16:51:22 -0500

    Add create post page

--- components/landing/Landing.js
+++ components/landing/Landing.js
@@ -7,6 +7,7 @@ import { useNavigation } from '@react-navigation/native';
 import EditProfile from '../profile/EditClientProfile';
 import { useState } from 'react';
 import EditVolunteerProfile from '../profile/EditVolunteerProfile';
+import CreatePost from '../post/CreatePost';
 
 export default function Landing() {
 	const navigation = useNavigation();
@@ -15,44 +16,43 @@ export default function Landing() {
 
 
 	return (
-		<EditVolunteerProfile />
-		// <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: BFTS_WHITE }}>
-		// 	<Image source={logo} style={landingStyles.logo} />
+		<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: BFTS_WHITE }}>
+			<Image source={logo} style={landingStyles.logo} />
 			
-		// 	<TouchableOpacity
-		// 		style={{
-		// 			backgroundColor: BFTS_WHITE,
-		// 			borderColor: BFTS_BLUE,
-		// 			width: 300,
-		// 			height: 65,
-		// 			justifyContent: 'center',
-		// 			alignItems: 'center',
-		// 			borderWidth: 3,
-		// 			borderRadius: 15,
-		// 			padding: 5,
-		// 		}}
-		// 		onPress={() => navigation.navigate('Login')}>
-		// 		<Text style={{ fontWeight: 'bold', fontSize: 19, fontFamily: 'Montserrat_700Bold' }}>Login</Text>
-		// 	</TouchableOpacity>
+			<TouchableOpacity
+				style={{
+					backgroundColor: BFTS_WHITE,
+					borderColor: BFTS_BLUE,
+					width: 300,
+					height: 65,
+					justifyContent: 'center',
+					alignItems: 'center',
+					borderWidth: 3,
+					borderRadius: 15,
+					padding: 5,
+				}}
+				onPress={() => navigation.navigate('Login')}>
+				<Text style={{ fontWeight: 'bold', fontSize: 19, fontFamily: 'Montserrat_700Bold' }}>Login</Text>
+			</TouchableOpacity>
 
-		// 	<TouchableOpacity
-		// 		style={{
-		// 			marginTop: 15,
-		// 			backgroundColor: BFTS_BLUE,
-		// 			borderColor: BFTS_WHITE,
-		// 			width: 300,
-		// 			height: 65,
-		// 			justifyContent: 'center',
-		// 			alignItems: 'center',
-		// 			borderWidth: 0,
-		// 			borderRadius: 15,
-		// 			padding: 5,
-		// 		}}
-		// 		onPress={() => navigation.navigate('Register')}>
-		// 		<Text style={{ fontWeight: 'bold', fontSize: 19, color: 'white', fontFamily: 'Montserrat_700Bold' }}>
-		// 			Register
-		// 		</Text>
-		// 	</TouchableOpacity>
-		// </View>
+			<TouchableOpacity
+				style={{
+					marginTop: 15,
+					backgroundColor: BFTS_BLUE,
+					borderColor: BFTS_WHITE,
+					width: 300,
+					height: 65,
+					justifyContent: 'center',
+					alignItems: 'center',
+					borderWidth: 0,
+					borderRadius: 15,
+					padding: 5,
+				}}
+				onPress={() => navigation.navigate('Register')}>
+				<Text style={{ fontWeight: 'bold', fontSize: 19, color: 'white', fontFamily: 'Montserrat_700Bold' }}>
+					Register
+				</Text>
+			</TouchableOpacity>
+		</View>
 	);
 }
